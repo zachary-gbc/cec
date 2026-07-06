a0d3b7fafdd82126230fa3a81e3c73240082fee4
Zachary Flight <zachary@grotonbiblechapel.org>
2026-07-06 17:09:42 -0400

    Added note for last import date

--- website/index.php
+++ website/index.php
@@ -13,6 +13,12 @@ while($row = mysqli_fetch_array($rs)) { $number=$row['CountEntries']; }
 if($number == 0) { echo("<h4>No Unviewed Entries</h4>\n"); }
 else { echo("<h4 style='color:red'>$number Unviewed Entries - <a href='checkentries.php'>View Entries</a></h4>\n"); }
 
+$lastimport="SELECT Var_Value FROM Variables WHERE (Var_System='$system') AND (Var_Name='last-import')"; $lastimportdate=0;
+if(!$rs=mysqli_query($db,$lastimport)) { echo("Unable to Run Query: $lastimport"); exit; }
+while($row = mysqli_fetch_array($rs)) { $lastimportdate=$row['Var_Value']; }
+
+echo("<h3>Last Import: " . date("F n, Y g:i a",strtotime($lastimportdate)) . "</h3>\n");
+
 ?>
 
 <?php include('other/footer.php'); ?>
