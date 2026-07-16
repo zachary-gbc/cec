5a88bfbcbbd944bfbc32ac8db72271db92be42ca
Zachary Flight <zachary@grotonbiblechapel.org>
2026-07-16 10:03:28 -0400

    Fixed date formatting

--- website/index.php
+++ website/index.php
@@ -17,7 +17,7 @@ $lastimport="SELECT Var_Value FROM Variables WHERE (Var_System='$system') AND (V
 if(!$rs=mysqli_query($db,$lastimport)) { echo("Unable to Run Query: $lastimport"); exit; }
 while($row = mysqli_fetch_array($rs)) { $lastimportdate=$row['Var_Value']; }
 
-echo("<h3>Last Import: " . date("F n, Y g:i a",strtotime($lastimportdate)) . "</h3>\n");
+echo("<h3>Last Import: " . date("F j, Y g:i a",strtotime($lastimportdate)) . "</h3>\n");
 
 ?>
 
